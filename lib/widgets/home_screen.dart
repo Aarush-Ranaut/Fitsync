4f4275f45b5b89351aae000e7240c269e49dd076
Amit69-hub <123amitvishwakarma@gmail.com>
2025-02-07 19:44:15 +0530

    CameraConnection_and_modelLoading

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -4,11 +4,15 @@ import 'package:fitsync_app/auth/signin.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'edit_profile_screen.dart'; // Import the EditProfileScreen
+import 'edit_profile_screen.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+import 'dart:typed_data';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key})
-      : super(key: key); // ✅ Removed username parameter
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -16,12 +20,156 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String? _profilePictureBase64;
-  String _firstName = ""; // ✅ Store first name in state
+  String _firstName = "";
+  late Interpreter _interpreter;
+  CameraController? _cameraController;
+  bool _isDetecting = false;
+  String _postureStatus = "Analyzing...";
+  bool _isModelLoaded = false; // Add a flag to track model loading
 
   @override
   void initState() {
     super.initState();
-    _fetchUserData(); // ✅ Load first name when screen starts
+    _fetchUserData();
+    _initializeCamera();
+    _loadModel();
+  }
+
+  // Load the model and set the flag when the model is ready
+  Future<void> _loadModel() async {
+    try {
+      _interpreter = await Interpreter.fromAsset(
+          'assets/models/exercise_form_model.tflite');
+      setState(() {
+        _isModelLoaded = true; // Set the flag to true when model is loaded
+      });
+      print("Model loaded successfully");
+    } catch (e) {
+      print("Error loading model: $e");
+    }
+  }
+
+  // Run inference only if the model is loaded
+  Future<void> _runInference(List<double> keypoints) async {
+    if (!_isModelLoaded || keypoints.length != 99) return;
+
+    try {
+      var input = [keypoints];
+      var output = List.filled(1, 0.0);
+      _interpreter?.run(input, output);
+
+      setState(() {
+        _postureStatus =
+            output[0] == 1 ? "Correct Posture" : "Incorrect Posture";
+      });
+    } catch (e) {
+      print("Error running inference: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    _interpreter.close();
+    super.dispose();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) {
+      print("No cameras available");
+      return;
+    }
+
+    _cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+
+    try {
+      await _cameraController?.initialize();
+      if (!mounted) return;
+      setState(() {});
+
+      _cameraController?.startImageStream((CameraImage image) {
+        if (!_isDetecting) {
+          _isDetecting = true;
+          _processFrame(image);
+        }
+      });
+    } catch (e) {
+      print("Error initializing camera: $e");
+    }
+  }
+
+  Future<void> _processFrame(CameraImage image) async {
+    if (_isDetecting) return;
+
+    _isDetecting = true;
+    final PoseDetector poseDetector =
+        PoseDetector(options: PoseDetectorOptions());
+    final InputImage inputImage = _convertCameraImage(image);
+
+    try {
+      final List<Pose> poses = await poseDetector.processImage(inputImage);
+      if (poses.isNotEmpty) {
+        List<double> keypoints = _extractKeypoints(poses.first);
+        await _runInference(keypoints);
+      }
+    } catch (e) {
+      print("Error processing frame: $e");
+    }
+
+    _isDetecting = false;
+  }
+
+  InputImage _convertCameraImage(CameraImage image) {
+    final List<int> bytesList = image.planes.fold<List<int>>(
+      [],
+      (previousValue, element) => previousValue + element.bytes,
+    );
+
+    final Uint8List bytes = Uint8List.fromList(bytesList);
+
+    final Size imageSize =
+        Size(image.width.toDouble(), image.height.toDouble());
+
+    final camera = _cameraController?.description;
+    final InputImageRotation rotation = camera != null
+        ? InputImageRotationValue.fromRawValue(camera.sensorOrientation) ??
+            InputImageRotation.rotation0deg
+        : InputImageRotation.rotation0deg;
+
+    final InputImageFormat format =
+        InputImageFormatValue.fromRawValue(image.format.raw) ??
+            InputImageFormat.nv21;
+
+    // Now we need to add 'bytesPerRow'
+    final bytesPerRow = image.planes[0].bytesPerRow;
+
+    return InputImage.fromBytes(
+      bytes: bytes,
+      metadata: InputImageMetadata(
+        size: imageSize,
+        rotation: rotation,
+        format: format,
+        bytesPerRow: bytesPerRow, // Add the 'bytesPerRow' here
+      ),
+    );
+  }
+
+  List<double> _extractKeypoints(Pose pose) {
+    List<double> keypoints = [];
+    for (PoseLandmarkType type in PoseLandmarkType.values) {
+      PoseLandmark? landmark = pose.landmarks[type];
+      if (landmark != null) {
+        keypoints.add(landmark.x);
+        keypoints.add(landmark.y);
+        keypoints.add(landmark.z);
+      } else {
+        keypoints.add(0.0);
+        keypoints.add(0.0);
+        keypoints.add(0.0);
+      }
+    }
+    return keypoints.length == 99 ? keypoints : List<double>.filled(99, 0.0);
   }
 
   Future<void> _fetchUserData() async {
@@ -34,11 +182,11 @@ class _HomeScreenState extends State<HomeScreen> {
             .get();
 
         if (userDoc.exists) {
-          String fullName = userDoc['firstName'] ?? 'Guest'; // Get full name
-          String firstName = fullName.split(' ')[0]; // Extract first name
+          String fullName = userDoc['firstName'] ?? 'Guest';
+          String firstName = fullName.split(' ')[0];
 
           setState(() {
-            _firstName = firstName; // ✅ Store first name in state variable
+            _firstName = firstName;
             _profilePictureBase64 = userDoc['profileImage'] ?? '';
           });
         }
@@ -51,7 +199,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void _logout(BuildContext context) {
-    FirebaseAuth.instance.signOut(); // Sign out the user
+    FirebaseAuth.instance.signOut();
     Navigator.pushAndRemoveUntil(
       context,
       MaterialPageRoute(builder: (context) => SigninScreen()),
@@ -164,184 +312,43 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(height: 20),
               Text(
                 'Jan 22, 2025',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                ),
+                style: TextStyle(color: Colors.white, fontSize: 14),
               ),
               Text(
                 _firstName,
                 style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
+                    color: Colors.green,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: Colors.green.withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Heart Rate',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            '69 BPM',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
+              Expanded(
+                child: Column(
+                  children: [
+                    _cameraController != null &&
+                            _cameraController!.value.isInitialized
+                        ? AspectRatio(
+                            aspectRatio: _cameraController!.value.aspectRatio,
+                            child: CameraPreview(_cameraController!),
+                          )
+                        : Center(child: CircularProgressIndicator()),
+                    SizedBox(height: 20),
+                    Text(
+                      _postureStatus,
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ),
-                  SizedBox(width: 16),
-                  Expanded(
-                    child: Stack(
-                      alignment: Alignment.centerRight,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            color: Colors.green.withOpacity(0.3),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Column(
-                            children: [
-                              Text(
-                                'Heart Rate',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Text(
-                                '69 BPM',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          right: 0,
-                          child: Icon(
-                            Icons.watch,
-                            size: 40,
-                            color: Colors.green,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 30),
-              Text(
-                'Tutorials',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                  ],
                 ),
               ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.white,
-                        image: DecorationImage(
-                          image: AssetImage('assets/back_workout.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          color: Colors.black.withOpacity(0.6),
-                          child: Text(
-                            'BACK',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 16),
-                  Expanded(
-                    child: Container(
-                      height: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.white,
-                        image: DecorationImage(
-                          image: AssetImage('assets/chest_workout.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          color: Colors.black.withOpacity(0.6),
-                          child: Text(
-                            'CHEST',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 16),
-                  Expanded(
-                    child: Container(
-                      height: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.green.withOpacity(0.3),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Spacer(),
               Align(
                 alignment: Alignment.bottomRight,
                 child: FloatingActionButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _runInference(List<double>.filled(99, 0.0));
+                  },
                   backgroundColor: Colors.green,
                   child: Icon(Icons.camera_alt, color: Colors.black),
                 ),
